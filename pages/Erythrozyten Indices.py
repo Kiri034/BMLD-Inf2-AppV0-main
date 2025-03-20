d47c867a1c90f839a001631a129fe692cb88a661
Kirisha <tharmkir@students.zhaw.ch>
2025-03-20 19:18:21 +0100

    exit

--- pages/Erythrozyten Indices.py
+++ pages/Erythrozyten Indices.py
@@ -1,31 +1,17 @@
 # ====== Start Login Block ======
 from utils.data_manager import DataManager
 from utils.login_manager import LoginManager
-import os
-import pandas as pd
-import streamlit as st
-from datetime import datetime
-
 LoginManager().go_to_login('Start.py') 
 # ====== End Login Block ======
 
-# Überprüfen, ob das Verzeichnis 'workspace' existiert, und erstelle es, falls nicht
-if not os.path.exists('workspace'):
-    os.makedirs('workspace')
-
-# Überprüfen, ob die Datei 'data.csv' existiert, und erstelle sie, falls nicht
-if not os.path.exists('workspace/data.csv'):
-    st.warning("Die Datei 'data.csv' existiert nicht. Eine leere Datei wird erstellt.")
-    df = pd.DataFrame(columns=['Datum', 'MCV', 'MCH', 'MCHC', 'Resultat'])
-    df.to_csv('workspace/data.csv', index=False)
-
-# Debugging: Überprüfe, ob die Datei existiert
-if os.path.exists('workspace/data.csv'):
-    st.write("Die Datei 'data.csv' existiert.")
-else:
-    st.error("Die Datei 'data.csv' existiert nicht. Bitte überprüfen Sie den Pfad.")
-
 # Here starts the actual app
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import io
+from datetime import datetime
+
+
 st.title("Erythrozyten Indices")
 
 # Initialize session state to store past values
@@ -75,12 +61,24 @@ if st.button("Analysieren", key="analyze_button", help="Klicken Sie hier, um die
         new_record = {'Datum': datetime.now(), 'MCV': mcv, 'MCH': mch, 'MCHC': mchc, 'Resultat': result}
         st.session_state.data.append(new_record)
 
-        # Debugging: Überprüfe den neuen Datensatz
-        st.write("Neuer Datensatz:", new_record)
-
         # update the data.csv file
-        DataManager().append_record(session_state_key='data', record_dict=new_record)
+        from utils.data_manager import DataManager
+        DataManager().append_record(session_state_key='data_df', record_dict=result)
+
 
         st.success("Daten erfolgreich gespeichert.")
     else:
         st.error("Bitte geben Sie gültige Werte für Hämoglobin, Erythrozytenzahl und Hämatokrit ein.")
+
+
+# CSS to style the button in red and make it smaller
+st.markdown("""
+    <style>
+    .stButton button {
+        background-color: red;
+        color: white;
+        font-size: 10px;  /* Reduced font size */
+        padding: 4px 8px;  /* Reduced padding */
+    }
+    </style>
+    """, unsafe_allow_html=True)
