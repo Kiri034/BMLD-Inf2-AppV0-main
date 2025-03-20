4e5766e7783fef01504349af330afd5e303ea89c
Kirisha <tharmkir@students.zhaw.ch>
2025-03-20 12:57:00 +0100

    Login - block

--- pages/Grafik.py
+++ pages/Grafik.py
@@ -1,3 +1,11 @@
+# ====== Start Login Block ======
+from utils.login_manager import LoginManager
+LoginManager().go_to_login('Start.py') 
+# ====== End Login Block ======
+
+
+# Here starts the graph page
+
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
