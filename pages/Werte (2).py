c8a83f680f99d9ddea25b5d8f9c1943007b1328b
Kiri034 <tharmkir@students.zhaw.ch>
2025-03-22 08:56:43 +0100

    Create Werte (2).py

---
+++ pages/Werte (2).py
@@ -0,0 +1,20 @@
+# ====== Start Login Block ======
+from utils.login_manager import LoginManager
+LoginManager().go_to_login('Start.py') 
+# ====== End Login Block ======
+
+# Überprüfen, ob die Session-State-Variable 'data' initialisiert ist
+import streamlit as st
+import pandas as pd
+
+st.title("Werte")
+
+st.write("Hier können Sie den Verlauf ihrer Resultate sehen.")
+
+# Überprüfe, ob Daten vorhanden sind
+if 'data' in st.session_state and st.session_state['data']:
+    df = pd.DataFrame(st.session_state['data'])
+    df['Datum'] = pd.to_datetime(df['Datum']).dt.strftime('%d.%m.%Y %H:%M:%S')
+    st.dataframe(df, use_container_width=True)
+else:
+    st.write("Es sind noch keine Resultate verfügbar.")
